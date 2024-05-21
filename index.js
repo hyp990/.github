53e7e3a5aed03813af73e32da82f7d6eee654af3
hyp <150382707+hyph90@users.noreply.github.com>
2024-05-21 21:08:50 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+import { Octokit } from "@octokit/core";
+import myPlugin from "./lib/my-plugin.js";
+import octokitPluginExample from "octokit-plugin-example";
+const MyOctokit = Octokit.plugin(
+  myPlugin,
+  octokitPluginExample
+);
+
+const octokit = new MyOctokit({ greeting: "Moin moin" });
+octokit.helloWorld(); // logs "Moin moin, world!"
+octokit.request("GET /"); // logs "GET / - 200 in 123ms"
+
+// lib/my-plugin.js
+const plugin = (octokit, options = { greeting: "Hello" }) => {
+  // hook into the request lifecycle
+  octokit.hook.wrap("request", async (request, options) => {
+    const time = Date.now();
+    const response = await request(options);
+    console.log(
+      `${options.method} ${options.url} – ${response.status} in ${Date.now() -
+        time}ms`
+    );
+    return response;
+  });
+
+  // add a custom method
+  return {
+    helloWorld: () => console.log(`${options.greeting}, world!`);
+  }
+};
+export default plugin;
